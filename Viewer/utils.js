82e40e1fbf42969e2b3f919361e1dc4cc047262e
Sunmin Son <smson@beyless.com>
2023-02-20 09:42:42 +0900

    fixed date bug

--- Viewer/utils.js
+++ Viewer/utils.js
@@ -821,8 +821,8 @@ function updateCurJunction(){
 function getTimestring(){
     let date = new Date();
     let year = date.getFullYear();
-    let month = ('0'+date.getMonth()).slice(-2);
-    let day = ('0'+date.getDay()).slice(-2);
+    let month = ('0'+(date.getMonth()+1)).slice(-2);
+    let day = ('0'+date.getDate()).slice(-2);
     let hour = ('0'+date.getHours()).slice(-2);
     let min = ('0'+date.getMinutes()).slice(-2);
     let sec = ('0'+date.getSeconds()).slice(-2);
