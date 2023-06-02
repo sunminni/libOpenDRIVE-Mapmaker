add1cdbf86c05e43a26b8c793103030200f680d5
Sunmin Son <smson@beyless.com>
2023-06-02 12:29:21 +0900

    junc 14

--- Viewer/main.js
+++ Viewer/main.js
@@ -483,8 +483,8 @@ function fitViewToBbox(bbox, restrict_zoom = false)
     // const dz = l2xy / Math.tan(fov2r);
     
     //K-CITY
-    center_pt.x = 560;
-    center_pt.y = 1400;
+    center_pt.x = 720;
+    center_pt.y = 1592;
     //KATECH
     // center_pt.x = 700;
     // center_pt.y = -340;
